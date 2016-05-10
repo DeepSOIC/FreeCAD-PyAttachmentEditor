c8b64e3ec47c85e8968a61ac699aecf1c69fde41
DeepSOIC <vv.titov@gmail.com>
2016-05-11 02:39:32 +0300

    progress

--- TaskAttachmentEditor.py
+++ TaskAttachmentEditor.py
@@ -1,10 +1,48 @@
 import FreeCAD as App
 import Part
+from FreeCAD import Units
+from Units import MilliMetre as mm
+from Units import Degree as deg
+from Units import Quantity as Q
 
 if App.GuiUp:
     import FreeCADGui as Gui
     from PySide import QtCore, QtGui
     from FreeCADGui import PySideUic as uic
+    
+def StrFromLink(feature, subname):
+    return feature.Name+ ((":"+subname) if subname else "")
+    
+def LinkFromStr(strlink, document):
+    if len(strlink) == 0:
+        return None
+    pieces = strlink.split(":")
+    
+    feature = document.getObject(pieces[0])
+    
+    subname = ""
+    if feature is None:
+        raise ValueError("No object named {name}".format(name= pieces[0]))
+    if len(pieces) == 2:
+        subname = pieces[1]
+    elif len(pieces) > 2:
+        raise ValueError("Failed to parse link (more than one colon encountered)")
+    
+    return (feature,subname)
+
+def StrListFromRefs(references):
+    '''input: PropertyLinkSubList. Output: list of strings for UI.'''
+    return [StrFromLink(feature,subelement) for (feature, subelement) in references]
+
+def RefsFromStrList(strings, document):
+    '''input: strings as from UI. Output: list of tuples that can be assigned to PropertyLinkSubList.'''
+    refs = []
+    for st in strings:
+        lnk = LinkFromStr(st, document)
+        if lnk is not None:
+            refs.append(lnk)
+    return refs
+        
 
 class AttachmentEditorTaskPanel:
     '''The editmode TaskPanel for attachment editing'''
@@ -17,11 +55,25 @@ class AttachmentEditorTaskPanel:
         else:
             raise TypeError("Object {objname} is not attachable. It has no Attacher attribute, and no AttacherType attribute"
                              .format(objname= obj_to_attach.Label))
-        self.attacher.readParametersFromFeature(obj_to_attach)
         
         import os
         self.form=uic.loadUi(os.path.dirname(__file__) + os.path.sep + "TaskAttachmentEditor.ui")
         # self.form.setWindowIcon(QtGui.QIcon(":/icons/PartDesign_InternalExternalGear.svg"))
+        
+        self.refLines = [self.form.lineRef1, 
+                         self.form.lineRef2,
+                         self.form.lineRef3,
+                         self.form.lineRef4]
+        self.refButtons = [self.form.buttonRef1,
+                           self.form.buttonRef2,
+                           self.form.buttonRef3,
+                           self.form.buttonRef4]
+                           
+        for i in range(len(self.refLines)):
+            QtCore.QObject.connect(self.refLines[i], QtCore.SIGNAL("textEdited(QString)"), lambda txt: self.lineRefChanged(i,txt))
+
+        for i in range(len(self.refLines)):
+            QtCore.QObject.connect(self.refButtons[i], QtCore.SIGNAL("clicked()"), lambda : self.refButtonClicked(i))
         
         QtCore.QObject.connect(self.form.superplacementX, QtCore.SIGNAL("valueChanged(double)"), self.superplacementXChanged)
         
@@ -45,35 +97,67 @@ class AttachmentEditorTaskPanel:
     
     def readParameters(self):
         "Transfer from the object to the dialog"
-        self.form.superplacementX.setValue(self.attacher.SuperPlacement.Base.x)
+        self.attacher.readParametersFromFeature(self.obj)
+        
+        plm = self.attacher.SuperPlacement
+        try:
+            self.form.blockSignals(True)
+            self.form.superplacementX.blockSignals(True)
+            self.form.superplacementX.setText    (Q(plm.Base.x, mm).UserString)
+            self.form.superplacementY.setText    (Q(plm.Base.y, mm).UserString)
+            self.form.superplacementZ.setText    (Q(plm.Base.z, mm).UserString)
+            self.form.superplacementYaw.setText  (Q(plm.Rotation.toEuler()[0], deg).UserString)
+            self.form.superplacementPitch.setText(Q(plm.Rotation.toEuler()[1], deg).UserString)
+            self.form.superplacementRoll.setText (Q(plm.Rotation.toEuler()[2], deg).UserString)
+            
+            strings = StrListFromRefs(self.attacher.References)
+            if len(strings) < len(self.refLines):
+                strings.extend([""]*(len(self.refLines) - len(strings)))
+            for i in range(len(self.refLines)):
+                self.refLines[i].setText(strings[i])
+        finally:
+            self.form.superplacementX.blockSignals(False)
+            self.form.superplacementX.blockSignals(False)
+            self.form.blockSignals(False)
         
     def superplacementXChanged(self, value):
         print value
         self.attacher.SuperPlacement.Base.x = value
         self.updatePreview()
         
+    def lineRefChanged(self, index, value):
+        self.updatePreview()
+            
+    def refButtonClicked(self, index):
+        print ("clicked button ",index)
+    
+    def parseAllRefLines(self):
+        self.attacher.References = RefsFromStrList([le.text() for le in self.refLines], self.obj.Document)
+        
     def updatePreview(self):
         new_plm = None
         try:
+            self.parseAllRefLines()
+            
             new_plm = self.attacher.calculateAttachedPlacement(self.obj.Placement)
+            if new_plm is None:
+                self.form.message.setText("Not attached")
+            else:
+                self.form.message.setText("Attached")
+                self.obj.Placement = new_plm
         except Exception as err:
             self.form.message.setText("Error: {err}".format(err= err.message))
-        if new_plm is None:
-            self.form.message.setText("Not attached")
-        else:
-            self.form.message.setText("Attached")
-            self.obj.Placement = new_plm
 
     def accept(self):
         #print 'accept(self)'
         self.writeParameters()
         self.obj.Document.commitTransaction()
-        FreeCADGui.Control.closeDialog()
+        Gui.Control.closeDialog()
                     
     def reject(self):
         #print 'reject(self)'
         self.obj.Document.abortTransaction()
-        FreeCADGui.Control.closeDialog()
+        Gui.Control.closeDialog()
 
 
 taskd = None
